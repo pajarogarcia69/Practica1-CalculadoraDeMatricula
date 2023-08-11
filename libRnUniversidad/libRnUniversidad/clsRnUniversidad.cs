fd3a29054221fa0b5a031f711a28a26a8a5ca0e1
pajarogarcia69 <stevenpajarogarcia@gmail.com>
2023-08-10 21:41:13 -0500

    add new metho

--- libRnUniversidad/libRnUniversidad/clsRnUniversidad.cs
+++ libRnUniversidad/libRnUniversidad/clsRnUniversidad.cs
@@ -3,37 +3,80 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.IO;
-
+using System.IO;  
 namespace libRnUniversidad
 {
     public class clsRnUniversidad
     {
         #region Atributos   
 
-        private int intTipoEst;
-        private float fltProm;
-
+        private int    intTipoEst;
+        private float  fltProm;
+        private float  fltValCredito;
+        private int    intCredit;
+        private float  fltDesc;
         private String strError;
         #endregion
 
-        #region Constructor         
+        #region Constructor
+        public clsRnUniversidad(int intTipoEst,float fltProm,float fltValCredito,int intCredit,float fltDesc,string strError)
+        {
+            intTipoEst = 0;
+            fltProm = 0;
+            fltValCredito = 0;
+            intCredit = 0;
+            fltDesc = 0;
+            strError = string.Empty;
+        }
         #endregion
 
         #region Propiedades   
-        public int tipoEstudiante
+        public int TipoEstudiante
          //Entrada
         {
             set { intTipoEst = value; }
         }
-
-        //
+        public float Promedio
+        {
+            set { fltProm = value; }
+        }
+        public float ValorCredito
+        {
+            get { return fltValCredito; }
+        }
+        public int NumCreditos
+        {
+            get { return intCredit; }
+        }
+        public float Descuento
+        {
+            get { return fltDesc; }
+        }
         public String Error
         {
             get { return strError;}
         }
         #endregion
-        #region Metodos Privados    
+
+        #region Metodos Privados  
+        private bool Validar()
+        {
+            if(intTipoEst != 1 && intTipoEst != 2)
+            {
+                strError = "Tipo de estudiante, no es valido";
+                return false;
+            }
+           
+            if (intTipoEst != 1 && intTipoEst != 2)
+            {
+                strError = "Tipo de estudiante, no es valido";
+                return false;
+            }
+                
+            return true;
+             
+        }
+
         private bool leerArchivo()
         {
             if (!Validar())
@@ -52,7 +95,7 @@ namespace libRnUniversidad
                 StreamReader Archivo = new StreamReader(@strPath);         // Crear objeto para leer el archivo
                 while ((strLinea = Archivo.ReadLine()) != null)             //  Leer linea por linea del archivo
                 {
-                    vectorLinea = strlinea.Split(':');
+                    vectorLinea = strLinea.Split(':');
                     strCodigo = vectorLinea[0];                            //Tipo de Estudiante (Programa)
                     fltNota = Convert.ToSingle(vectorLinea[1]);             //Promedio Mínimo de Nota
                     if (strCodigo == intTipoEst.ToString() && fltProm >= fltNota)
@@ -71,9 +114,15 @@ namespace libRnUniversidad
                 strError = ex.Message;
                 return false;
             }
-        }
+        } 
         #endregion
-        #region Metodos Publicos         
+
+        #region Metodos Publicos     
+        public bool hallarDatos()
+        {
+            return leerArchivo();
+        }
+
         #endregion
     }
 }
