a88038962cdb1cb41cf0f6f1f5276d5bafbbf9dd
pajarogarcia69 <stevenpajarogarcia@gmail.com>
2023-08-13 15:14:20 -0500

    Agregamos la parte visual de nuestro proyecto llamada appPractica1

--- libOpeUniversidad/libOpeUniversidad/clsOpeUniversidad.cs
+++ libOpeUniversidad/libOpeUniversidad/clsOpeUniversidad.cs
@@ -3,10 +3,84 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+// using System.libRnUniversidad;
 
 namespace libOpeUniversidad
 {
-    public class Class1
+    public class clsOpeUniversidad
     {
+        #region Atributos
+
+        private int    intTipoEst;
+        private int    intCredit;
+        private float  fltProm;
+        private float  fltValorCred;
+        private float  fltValDescuento;
+        private float  fltValorPago;
+        private string strError;
+
+        #endregion
+
+        #region Constructor
+
+        public clsOpeUniversidad(int intTipoEst, int intCredit, float fltProm, float fltValorCred, float fltValDescuento, float fltValorPago, string strError)
+        {
+            intTipoEst      = 0;
+            intCredit       = 0;
+            fltProm         = 0;
+            fltValorCred    = 0;
+            fltValDescuento = 0;
+            fltValorPago    = 0;
+            strError        = String.Empty;
+        }
+
+        #endregion
+
+        #region Propiedades
+        
+        public int TipoEstudiante
+        {
+            set { intTipoEst = value; }
+            get { return intTipoEst;  }
+        }
+
+        public int NumCreditos
+        {
+            set { intCredit = value; }
+            get { return intCredit;  }
+        }
+
+        public float Promedio
+        {
+            get { return fltProm; }
+        }
+
+        public float ValorCred
+        {
+            get { return fltValorCred; }
+        }
+
+        public float Descuento
+        {
+            get { return fltValDescuento; }
+        }
+
+        public float ValorPago
+        {
+            get { return fltValorPago; }
+        }
+
+        public string Error
+        {
+            get { return strError; }
+        }
+
+        #endregion
+
+        #region Metodos Privados
+        #endregion
+
+        #region Metodos Publicos
+        #endregion
     }
 }
